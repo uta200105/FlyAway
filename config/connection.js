cb92e7334cdfcabad3365f14137ef6414cd70641
A Gautreaux <armando.gautreaux@icloud.com>
2018-09-23 03:33:20 -0500

    Created Routes and Handlebars

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  port: 'YOUR PORT',
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'FlyAway_db'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  } else {
+    console.log('connected as id ' + connection.threadId);
+  }
+});
+
+module.exports = connection;
---
+++ models/jets.js
@@ -0,0 +1,38 @@
+var orm = require('../config/orm.js');
+var jets = {
+  findJoinTables: function(condition1, condition2, condition3, cb) {
+    orm.findJoinTables(
+      'bookings',
+      'flights',
+      'flight_id',
+      condition1,
+      condition2,
+      condition3,
+      function(res) {
+        cb(res);
+      }
+    );
+  },
+  selectWhere: function(condition1, condition2, cb) {
+    orm.selectWhere('flights', condition1, condition2, function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.create('bookings', cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(condition1, condition, cb) {
+    orm.update('flights', condition1, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete('bookings', condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = jets;
